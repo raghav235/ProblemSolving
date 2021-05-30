b581be80c9f0e6a06aabb40c1cce8afd142441d7
Raghav Mishra <Raghav.Mishra@oracle.com>
2021-05-30 17:32:48 +0530

    Commit 2

--- src/com/learning/git/HelloWorld.java
+++ src/com/learning/git/HelloWorld.java
@@ -3,5 +3,7 @@ package com.learning.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to learning!!");
+
+        System.out.println("Learning is a continuous process.");
     }
 }
